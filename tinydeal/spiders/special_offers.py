b531f8a9f8394b29d800878970d023790e63cbd8
Yash Mishra <yashmishra12@hotmail.com>
2020-08-24 20:30:13 +0530

    crawled first page products

--- tinydeal/spiders/special_offers.py
+++ tinydeal/spiders/special_offers.py
@@ -7,4 +7,10 @@ class SpecialOffersSpider(scrapy.Spider):
     start_urls = ['https://www.tinydeal.com/specials.html']
 
     def parse(self, response):
-        pass
+        for product in response.xpath("//ul[@class='productlisting-ul']/div/li"):
+            yield {
+                'title' : product.xpath(".//a[@class='p_box_title']/text()").get(),
+                'url' : response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
+                'discounted_price' : product.xpath(".//div[@class='p_box_price']/span[1]/text()").get(),
+                'original_price' : product.xpath(".//div[@class='p_box_price']/span[2]/text()").get()
+            }
